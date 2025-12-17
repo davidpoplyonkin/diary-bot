45ce05fdd7e9839cd27cbab7a2430eea79663447
Davyd Poplyonkin <dp.8cbpm@gmail.com>
2025-12-17 14:02:45 +0200

    Feat: Connect aioscheduler to a Postgres job store

--- bot/main.py
+++ bot/main.py
@@ -1,14 +1,23 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 
-from globals import TG_TOKEN, ADMIN_TG_ID, IANA_TZ
+from globals import (TG_TOKEN, ADMIN_TG_ID, IANA_TZ, POSTGRES_PASSWORD,
+                     POSTGRES_USER)
 from apps import core, diary, notifications
 
 bot = Bot(token=TG_TOKEN)
 dp = Dispatcher()
 
-scheduler = AsyncIOScheduler(timezone=IANA_TZ)
+jobstores = {
+    "default": SQLAlchemyJobStore(url=(
+        f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
+        "@postgres:5432/scheduler"
+    ))
+}
+
+scheduler = AsyncIOScheduler(timezone=IANA_TZ, jobstores=jobstores)
 
 async def main():
     # Create tables
@@ -22,6 +31,8 @@ async def main():
         diary.router,
         core.router,
     )
+
+    scheduler.start()
 
     # Notify the admin that the bot has started
     await bot.send_message(chat_id=ADMIN_TG_ID, text="START")
